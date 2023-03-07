14b04d3e8d06397bd94fb7cd33228650e8294b22
david bahtiar <52740811+davidbahtiar55@users.noreply.github.com>
2023-03-07 20:36:33 +0700

    Add Scenario

---
+++ RestAssuredTestDemo/src/test/java/steps/GetPostSteps.java
@@ -0,0 +1,18 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class GetPostSteps {
+    @Given("I perform GET operation for {string}")
+    public void i_perform_get_operation_for(String string) {
+    }
+    @Given("I perform GET for the post number {string}")
+    public void i_perform_get_for_the_post_number(String string) {
+
+    }
+    @Then("I should see the author name {string}")
+    public void i_should_see_the_author_name(String string) {
+
+    }
+}
